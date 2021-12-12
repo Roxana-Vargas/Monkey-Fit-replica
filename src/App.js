c6363fe3d773d56db05600d32ccae2af440ae46d
Roxana Vargas <roxvargasc@gmail.com>
2021-12-11 21:59:13 -0500

    Add NavBar offCanvas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import "./app.css"
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      Monkey Fit
+      <div className="d-flex justify-content-between">
+        <img className="logo" src={"https://www.monkeyfitpass.com/_nuxt/img/new-logo.f8a84a8.png"} alt="logo"></img>
+        <NavBar />
+      </div>
     </div>
   );
 }
