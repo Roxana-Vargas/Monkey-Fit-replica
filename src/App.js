d04a387fade0b2fb135303385c56778a4b402140
Roxana Vargas <roxvargasc@gmail.com>
2021-12-12 15:42:12 -0500

    Add section details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import "./app.css"
 import Banner from "./components/Banner";
+import Details from "./components/Details";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <div className="container-navbar d-flex justify-content-between px-5">
+      <section className="container-navbar d-flex justify-content-between px-5">
         <img className="logo" src={"https://www.monkeyfitpass.com/_nuxt/img/new-logo.f8a84a8.png"} alt="logo"></img>
         <div className="d-flex">
           <a href="#iniciar-sesión" className="url-login my-4" >Iniciar sesión</a>
           <NavBar />
         </div>
-      </div>
-      <div className="row container-banner">
+      </section>
+      <section className="container-banner row">
         <div className="col-sm-12 col-lg-6">
           <Banner />
         </div>
         <div className="col-sm-12 col-lg-6">
           <img className="img-banner" src={'https://www.monkeyfitpass.com/_nuxt/img/hero-employees.dab5e1a.png'} alt='Employees MokeyFit'/>
         </div>
-        
-      </div>
+      </section>
+      <section className="container-details">
+        <Details />
+      </section>
     </div>
   );
 }
---
+++ src/components/Details.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './details.css'
+
+const Details = () => {
+    return (
+        <div className='details'>
+            <h2 className='title-details'>Una plataforma que te ayuda a <span>mantenerte activo</span> y <span>saludable</span> estés donde estés</h2>
+            <div  className='container-details'>
+                <div className='container-image-details'>
+                    <img src={'https://www.monkeyfitpass.com/_nuxt/img/hero-two.9c54df0.svg'} alt="Entrena en casa" />
+                    <button className='btn btn-start-now'>Empieza ahora</button>
+                </div>
+                <div className='container-cards'>
+                    <div className='card-detail d-flex justify-content-evenly align-items-center'>
+                        <div className='image-detail'>
+                            <img src={'https://www.monkeyfitpass.com/_nuxt/img/video.3866e95.svg'} alt="" />
+                        </div>
+                        <div className='container-text-detail'>
+                            <p className='text-detail'>Clases grabadas y en vivo de tus entrenadores favoritos</p>    
+                        </div>
+                    </div>
+                    <div className='card-detail d-flex justify-content-evenly align-items-center'>
+                        <div className='image-detail'>
+                            <img src={'https://www.monkeyfitpass.com/_nuxt/img/pass.efd1dcc.svg'} alt="" />
+                        </div>
+                        <div className='container-text-detail'>
+                            <p className='text-detail'>Membresía mensual asumidad por tu empresa</p> 
+                        </div>
+                    </div>
+                    <div className='card-detail d-flex justify-content-evenly align-items-center'>
+                        <div className='image-detail'>
+                            <img src={'https://www.monkeyfitpass.com/_nuxt/img/person.6dc9a84.svg'} alt="" />
+                        </div>
+                        <div className='container-text-detail'>
+                            <p className='text-detail'>Gratis para ti y 70% de descuento para tus familiares</p> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Details
